0bda426eab8573ddb345f742306034cb260bf1fe
Diego Armando Gomez <diegomezusb@gmail.com>
2021-04-17 08:29:06 -0500

    Creacion del Controller

--- src/main/java/co/edu/eafit/bank/controller/CustomerController.java
+++ src/main/java/co/edu/eafit/bank/controller/CustomerController.java
@@ -1,9 +1,13 @@
 package co.edu.eafit.bank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.eafit.bank.dto.CustomerDTO;
 import co.edu.eafit.bank.service.CustomerService;
 
 @RestController
@@ -13,5 +17,9 @@ public class CustomerController {
 	@Autowired
 	CustomerService customerService;
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<CustomerDTO> findById(@PathVariable("id") Integer id){
+		return null;
+	}
 		
 }
