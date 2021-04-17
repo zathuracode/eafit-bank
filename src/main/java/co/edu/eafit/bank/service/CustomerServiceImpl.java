8bb02f20c63d3fbb12f10f7703e23ad70d25aa5b
Diego Armando Gomez <diegomezusb@gmail.com>
2021-04-16 20:54:03 -0500

    Creacion del Customer Service

--- src/main/java/co/edu/eafit/bank/service/CustomerServiceImpl.java
+++ src/main/java/co/edu/eafit/bank/service/CustomerServiceImpl.java
@@ -10,7 +10,6 @@ import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
