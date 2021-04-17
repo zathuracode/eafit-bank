d44f11d71efbaf504edf01855cddbbdae040f71f
Diego Armando Gomez <diegomezusb@gmail.com>
2021-04-17 08:36:28 -0500

    Creacion del Controller

--- src/main/java/co/edu/eafit/bank/controller/CustomerController.java
+++ src/main/java/co/edu/eafit/bank/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package co.edu.eafit.bank.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.eafit.bank.domain.Customer;
 import co.edu.eafit.bank.dto.CustomerDTO;
 import co.edu.eafit.bank.service.CustomerService;
 
@@ -19,7 +22,22 @@ public class CustomerController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<CustomerDTO> findById(@PathVariable("id") Integer id){
-		return null;
+		
+		Optional<Customer> customerOptional=customerService.findById(id);
+		
+		Customer customer=(customerOptional.isPresent()==true)?customerOptional.get():null;
+		
+		CustomerDTO customerDTO=new CustomerDTO();
+		customerDTO.setAddress(customer.getAddress());
+		customerDTO.setCustId(customer.getCustId());
+		customerDTO.setDotyId(customer.getDocumentType().getDotyId());
+		customerDTO.setEmail(customer.getEmail());
+		customerDTO.setEnable(customer.getEnable());
+		customerDTO.setName(customer.getName());
+		customerDTO.setPhone(customer.getPhone());
+		customerDTO.setToken(customer.getToken());
+		
+		return ResponseEntity.ok().body(customerDTO);
 	}
 		
 }
